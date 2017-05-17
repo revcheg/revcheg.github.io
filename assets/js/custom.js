9943c192abd28faf840c427f3dcda97304523aa5
revcheg <revcheg0@gmail.com>
2017-05-17 21:16:19 +0300

    Правки стилей

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,11 @@
 (function($){
 
-	/* ---------------------------------------------- /*
-	 * Preloader
-	/* ---------------------------------------------- */
-
 	$(window).load(function() {
 		$('#status').fadeOut();
 		$('#preloader').delay(300).fadeOut('slow');
 	});
 
 	$(document).ready(function() {
-
-		/* ---------------------------------------------- /*
-		 * Smooth scroll / Scroll To Top
-		/* ---------------------------------------------- */
 
 		$('a[href*=#]').bind("click", function(e){
            
@@ -32,10 +24,6 @@
 			}
 		});
 
-		/* ---------------------------------------------- /*
-		 * Navbar
-		/* ---------------------------------------------- */
-
 		$('.header').sticky({
 			topSpacing: 0
 		});
@@ -44,12 +32,6 @@
 			target: '.navbar-custom',
 			offset: 70
 		})
-
-        
-        /* ---------------------------------------------- /*
-		 * Skills
-        /* ---------------------------------------------- */    
-        //var color = $('#home').css('backgroundColor');
 
         $('.skills').waypoint(function(){
             $('.chart').each(function(){
@@ -64,37 +46,6 @@
                 });
             });
         },{offset:'80%'});
-        
-        
-        /* ---------------------------------------------- /*
-		 * Quote Rotator
-		/* ---------------------------------------------- */
-       
-			$( function() {
-				/*
-				- how to call the plugin:
-				$( selector ).cbpQTRotator( [options] );
-				- options:
-				{
-					// default transition speed (ms)
-					speed : 700,
-					// default transition easing
-					easing : 'ease',
-					// rotator interval (ms)
-					interval : 8000
-				}
-				- destroy:
-				$( selector ).cbpQTRotator( 'destroy' );
-				*/
-
-				$( '#cbp-qtrotator' ).cbpQTRotator();
-
-			} );
-		
-        
-		/* ---------------------------------------------- /*
-		 * Home BG
-		/* ---------------------------------------------- */
 
 		$(".screen-height").height($(window).height());
 
@@ -108,29 +59,15 @@
 			$('#home').parallax('50%', 0.1);
 		}
 
-
-		/* ---------------------------------------------- /*
-		 * WOW Animation When You Scroll
-		/* ---------------------------------------------- */
-
 		wow = new WOW({
 			mobile: false
 		});
 		wow.init();
 
-
-		/* ---------------------------------------------- /*
-		 * E-mail validation
-		/* ---------------------------------------------- */
-
 		function isValidEmailAddress(emailAddress) {
 			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 			return pattern.test(emailAddress);
 		};
-
-		/* ---------------------------------------------- /*
-		 * Contact form ajax
-		/* ---------------------------------------------- */
 
 		$('#contact-form').submit(function(e) {
 
@@ -154,10 +91,10 @@
 
 			else {
 					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
+							type        : 'POST',
+							url         : 'assets/php/contact.php',
+							data        : formData,
+							dataType    : 'json',
 							encode      : true,
 							success		: function(res){
 											var ret = $.parseJSON(JSON.stringify(res));
