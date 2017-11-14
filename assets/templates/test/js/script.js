440c6e5e8ac54face371362e7c35dfc35c816661
Max Sharing <revcheg0@gmail.com>
2017-11-14 15:28:40 +0200

    s

---
+++ assets/templates/test/js/script.js
@@ -0,0 +1,24 @@
+var par1 = document.querySelector(".hide-1");
+var par2 = document.querySelector(".hide-2");
+var par3 = document.querySelector(".hide-3");
+var read = document.querySelector(".read");
+var link1 = document.querySelector(".open-1").onclick = function (e) {
+  e.preventDefault();
+  par1.style.display = "block";
+  read.style.minHeight = "440px";
+  read.style.transition = "0.1s";
+};
+
+var link2 = document.querySelector(".open-2").onclick = function (e) {
+  e.preventDefault();
+  par2.style.display = "block";
+  read.style.minHeight = "440px";
+  read.style.transition = "0.1s";
+};
+
+var link3 = document.querySelector(".open-3").onclick = function (e) {
+  e.preventDefault();
+  par3.style.display = "block";
+  read.style.minHeight = "440px";
+  read.style.transition = "0.1s";
+};
