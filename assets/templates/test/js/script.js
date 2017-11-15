7325f03a9b6fd82d100b3c253aa4fe87b424f4a5
Max Sharing <revcheg0@gmail.com>
2017-11-15 18:55:13 +0200

    y

--- assets/templates/test/js/script.js
+++ assets/templates/test/js/script.js
@@ -5,20 +5,20 @@ var read = document.querySelector(".read");
 var link1 = document.querySelector(".open-1").onclick = function (e) {
   e.preventDefault();
   par1.style.display = "block";
-  read.style.minHeight = "440px";
+  read.style.minHeight = "400px";
   read.style.transition = "0.1s";
 };
 
 var link2 = document.querySelector(".open-2").onclick = function (e) {
   e.preventDefault();
   par2.style.display = "block";
-  read.style.minHeight = "440px";
+  read.style.minHeight = "400px";
   read.style.transition = "0.1s";
 };
 
 var link3 = document.querySelector(".open-3").onclick = function (e) {
   e.preventDefault();
   par3.style.display = "block";
-  read.style.minHeight = "440px";
+  read.style.minHeight = "400px";
   read.style.transition = "0.1s";
 };
