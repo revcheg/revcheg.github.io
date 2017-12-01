b2f2c08b0a1281ceffd969c2d35c4a23e43467c0
Max Sharing <revcheg0@gmail.com>
2017-12-01 17:24:11 +0200

    a

---
+++ assets/templates/crypto-b/js/script.js
@@ -0,0 +1,7 @@
+// var reg = document.querySelector('.test-v');
+// var log = document.querySelector('.test-h');
+// var link = document.querySelector('.link').onclick = function (e) {
+//   e.preventDefault();
+//   reg.style.display = 'none';
+//   log.style.display = 'block';
+// };
