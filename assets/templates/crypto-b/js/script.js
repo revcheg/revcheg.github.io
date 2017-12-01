fc8e69ad613626618f161f319c87b56ceb20888e
Max Sharing <revcheg0@gmail.com>
2017-12-01 17:35:57 +0200

    add script

--- assets/templates/crypto-b/js/script.js
+++ assets/templates/crypto-b/js/script.js
@@ -1,7 +1,14 @@
-// var reg = document.querySelector('.test-v');
-// var log = document.querySelector('.test-h');
-// var link = document.querySelector('.link').onclick = function (e) {
-//   e.preventDefault();
-//   reg.style.display = 'none';
-//   log.style.display = 'block';
-// };
+var registr = document.querySelector('.page-header__form--sign-up');
+var login = document.querySelector('.page-header__form--login');
+
+var signIn = document.querySelector('.page-header__login').onclick = function (e) {
+  e.preventDefault();
+  registr.style.display = 'none';
+  login.style.display = 'flex';
+};
+
+var signUp = document.querySelector('.page-header__sign-up').onclick = function (e) {
+  e.preventDefault();
+  login.style.display = 'none';
+  registr.style.display = 'flex';
+};
