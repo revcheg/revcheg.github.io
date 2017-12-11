cdfcef1575d553f1d424197d4a87c99b8ac6a00f
Max Sharing <revcheg0@gmail.com>
2017-12-11 15:40:44 +0200

    s

--- assets/templates/forex/js/script.js
+++ assets/templates/forex/js/script.js
@@ -4,3 +4,18 @@ $('.slider__box').slick({
   arrows: false,
   dots: true
 });
+
+var navMain = document.querySelector('.page-header__main-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('page-header__main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('page-header__main-nav--closed')) {
+    navMain.classList.remove('page-header__main-nav--closed');
+    navMain.classList.add('page-header__main-nav--opened');
+  } else {
+    navMain.classList.add('page-header__main-nav--closed');
+    navMain.classList.remove('page-header__main-nav--opened');
+  }
+});
