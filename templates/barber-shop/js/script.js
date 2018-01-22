4eb9394fc146defdcf1667bf6f5ccd017dfb1a33
Max Sharing <revcheg0@gmail.com>
2018-01-22 14:59:41 +0200

    ch

---
+++ templates/barber-shop/js/script.js
@@ -0,0 +1,90 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+var link = document.querySelector(".main-nav__user-login");
+var popup = document.querySelector(".modal-login");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var form = popup.querySelector("form");
+var close = document.querySelector(".modal-login__closed");
+var storage = localStorage.getItem("login");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-login--hide");
+  popup.classList.add("modal-login--show");
+  login.focus();
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-login--show");
+  popup.classList.remove("modal-login--error");
+  popup.classList.add("modal-login--hide");
+  popup.classList.remove("modal-login--static");
+});
+
+form.addEventListener("submit", function(event) {
+  if (!login.value || !password.value) {
+    event.preventDefault();
+    popup.classList.add("modal-login--error");
+    popup.classList.add("modal-login--static");
+    var pew = setTimeout(function(){
+          popup.classList.remove("modal-login--hide");
+          popup.classList.remove("modal-login--show");
+          popup.classList.remove("modal-login--error");
+          clearTimeout(pew);
+    }, 600);
+  } else {
+      localStorage.setItem("login", login.value);
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    popup.classList.remove("modal-login--show");
+    popup.classList.remove("modal-login--error");
+    popup.classList.add("modal-login--hide");
+    popup.classList.remove("modal-login--static");
+  }
+});
+
+var entryForm = document.querySelector(".entry__form");
+var name = entryForm.querySelector("[name=name]");
+var email = entryForm.querySelector("[name=email]");
+var modal = document.querySelector(".modal");
+var modalFail = document.querySelector(".modal--fail");
+var modalSuccess = document.querySelector(".modal--success");
+var modalClose = modal.querySelector(".modal__closed");
+
+entryForm.addEventListener("submit", function(event) {
+  if (!name.value || !email.value) {
+    event.preventDefault();
+    modalFail.classList.remove("modal--hide");
+    modalFail.classList.add("modal--show");
+  }
+});
+
+modalClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  modalFail.classList.remove("modal--show");
+  modalFail.classList.add("modal--hide");
+});
