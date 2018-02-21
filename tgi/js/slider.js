0aee01a26b014c0ad933c1778e035acdbb702188
Max Sharing <revcheg0@gmail.com>
2018-02-21 12:51:15 +0200

    slider

--- tgi/js/slider.js
+++ tgi/js/slider.js
@@ -1,8 +1,12 @@
 $('.slider__content').slick({
   autoplay: false,
   autoplaySpeed: 2000,
+  vertical: true,
   arrows: false,
   dots: true,
+  customPaging: function (slider, i) {
+    return i + 1;
+  },
   responsive: [{
     breakpoint: 768,
     settings: {
