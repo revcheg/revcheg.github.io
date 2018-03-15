22300fd8369435b5fa1d12bcfb908e043156a589
Max Sharing <maxsharing@Maxs-MacBook-Pro.local>
2018-03-15 13:59:55 +0200

    slider

---
+++ borsa/js/slider.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+  autoplay: false,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: false,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      autoplay: false
+    }
+  }]
+});
