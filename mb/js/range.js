f2d7c08deffea01eb8046e85dde306cc04562274
Max Sharing <maxsharing@Maxs-MacBook-Pro.local>
2018-04-02 18:05:04 +0300

    add

---
+++ mb/js/range.js
@@ -0,0 +1,4 @@
+function range(){
+	var val = $('.ranges').val();
+	$('.ranges').css({'background':'-webkit-linear-gradient(left ,#002e5f 0%,#000 '+val+'%,#ddd '+val+'%, #ddd 100%)'});
+ }
---
+++ mb/js/time.js
@@ -0,0 +1,5 @@
+// var wetBox = document.querySelector('.time__box--wet');
+// 
+// var wetTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
+// 
+// wetBox.innerHTML = wetTime;
