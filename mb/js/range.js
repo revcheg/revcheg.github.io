929c014497e9bd42e2ba51c7e61a2dd91e4c3df8
Max Sharing <maxsharing@Maxs-MacBook-Pro.local>
2018-04-03 15:08:46 +0300

    js

--- mb/js/range.js
+++ mb/js/range.js
@@ -1,4 +1,26 @@
-function range(){
-	var val = $('.ranges').val();
-	$('.ranges').css({'background':'-webkit-linear-gradient(left ,#002e5f 0%,#000 '+val+'%,#ddd '+val+'%, #ddd 100%)'});
- }
+var rangeInvest = document.querySelector(".range__invest");
+var rangeGet = document.querySelector(".range__get");
+
+var progressInvest = document.querySelector('.progress__invest');
+var progressInvestOutput = document.querySelector('.range__invest-output');
+
+var progressGet = document.querySelector('.progress__get');
+var progressGetOutput = document.querySelector('.range__get-output');
+
+function invest() {
+	var val = $('.range__invest').val();
+
+	$('.range__invest').css({'background':'-webkit-linear-gradient(left ,#002e5f 0%,#000 '+(val*100)/1000000+'%,#ddd '+(val*100)/1000000+'%, #ddd 100%)'});
+
+	progressInvest.value = rangeInvest.value;
+	progressInvestOutput.innerHTML = rangeInvest.value + ' USD';
+};
+
+function get() {
+	var val = $('.range__get').val();
+
+	$('.range__get').css({'background':'-webkit-linear-gradient(left, #002e5f 0%, #000 '+(val*100)/12+'%, #ddd '+(val*100)/12+'%, #ddd 100%)'});
+
+	progressGet.value = rangeGet.value;
+	progressGetOutput.innerHTML = rangeGet.value + ' USD';
+};
