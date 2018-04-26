f50fb48293ded8525c9268e4561abe591cf4366c
Max Sharing <revcheg0@gmail.com>
2018-04-26 16:24:41 +0300

    zulu fucking trade, i hate this job

--- forexglobal/js/main.js
+++ forexglobal/js/main.js
@@ -238,3 +238,19 @@ zulutradeButton.addEventListener('click', function () {
   zulutradeVideo.play();
   zulutradeButton.style.opacity = '0';
 });
+
+// Accordion
+var acc = document.getElementsByClassName("zulu__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+};
