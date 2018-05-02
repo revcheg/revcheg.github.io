509d4cfbfdfa9549b6581b71c14dabf7ab8d4351
Max Sharing <revcheg0@gmail.com>
2018-05-02 16:30:52 +0300

    mobile vers

--- prizma/js/main.js
+++ prizma/js/main.js
@@ -1,19 +1,3 @@
-// Nav
-var navItem = document.querySelector('.nav__item');
-var navSub = document.querySelector('.nav__sublist');
-
-navItem.addEventListener('click', function () {
-  if (navItem.classList.contains('nav__item--inactive')) {
-    navItem.classList.remove('nav__item--inactive');
-    navItem.classList.add('nav__item--active');
-    navSub.classList.add('nav__sublist--active');
-  } else {
-    navItem.classList.remove('nav__item--active');
-    navItem.classList.add('nav__item--inactive');
-    navSub.classList.remove('nav__sublist--active');
-  }
-});
-
 // Slick
 $('.slider').slick({
   dots: true,
@@ -24,30 +8,15 @@ $('.slider').slick({
   responsive: [{
     breakpoint: 1024,
     settings: {
-      slidesToShow: 3,
-      infinite: true
+      // slidesToShow: 3,
     }
   }, {
     breakpoint: 600,
     settings: {
-      slidesToShow: 2,
-      dots: true
+      // slidesToShow: 2,
     }
   }, {
     breakpoint: 300,
     settings: 'uncslick'
   }]
 });
-
-// $('.slider').slick({
-//   autoplay: true,
-//   autoplaySpeed: 2000,
-//   arrows: false,
-//   dots: true,
-//   responsive: [{
-//     breakpoint: 768,
-//     settings: {
-//       autoplay: false
-//     }
-//   }]
-// });
