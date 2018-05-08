7b4fa85f54f789e0076d6813a0f507dad40a6f99
Max Sharing <revcheg0@gmail.com>
2018-05-08 16:56:43 +0300

    edits for fucking investors

--- prizma/js/main.js
+++ prizma/js/main.js
@@ -63,6 +63,23 @@ for (var i = 0; i < hoverPremium.length; i++) {
   });
 };
 
+// Accordion
+var acc = document.getElementsByClassName('glossary__name');
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+    this.classList.toggle('glossary__name--active');
+    
+    var panel = this.nextElementSibling;
+    
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }; 
+  });
+};
+
 // Slick
 $('.slider').slick({
   dots: true,
