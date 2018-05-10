9a32525695983e31c7530728d2ee71c83ecf4111
Max Sharing <revcheg0@gmail.com>
2018-05-10 15:56:12 +0300

    links to register page

--- prizma/js/main.js
+++ prizma/js/main.js
@@ -95,26 +95,3 @@ switchButton.addEventListener('click', function () {
     passwordInput.setAttribute('type', 'password');
   };
 });
-
-// Slick
-$('.slider').slick({
-  dots: true,
-  arrows: false,
-  customPaging: function (slider, i) {
-    return i + 1;
-  },
-  responsive: [{
-    breakpoint: 1024,
-    settings: {
-      // slidesToShow: 3,
-    }
-  }, {
-    breakpoint: 600,
-    settings: {
-      // slidesToShow: 2,
-    }
-  }, {
-    breakpoint: 300,
-    settings: 'uncslick'
-  }]
-});
