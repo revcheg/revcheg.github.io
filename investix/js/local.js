56e78378cb678f31ec6631b4a8cd8426831eb166
Max Sharing <revcheg0@gmail.com>
2018-06-04 17:41:10 +0300

    add localize

---
+++ investix/js/local.js
@@ -0,0 +1,10 @@
+var dict = {
+  en: {
+    'slider-title': 'Day Trading Education Package',
+    'slider-text': 'Pay only for trading education and receive<br>all the trading capital you need'
+  },
+  ru: {
+    'slider-title': 'Заголовок сайта',
+    'slider-text': 'Нужно больше текста'
+  }
+};
