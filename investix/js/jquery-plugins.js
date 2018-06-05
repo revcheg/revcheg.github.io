b859cf67caded3b92fcd0bce521f6d57af3c9e1f
Max Sharing <revcheg0@gmail.com>
2018-06-05 12:14:10 +0300

    s

--- investix/js/jquery-plugins.js
+++ investix/js/jquery-plugins.js
@@ -5,19 +5,3 @@ $('.footer__up').click(function(event){
     }, 1000);
     event.preventDefault();
 });
-
-// Local
-var select = document.querySelector('.nav__select');
-
-$.html5Translate = function(dict, lang){
-  $('[data-translate-key]').each(function(){
-    $(this).html( dict[ lang ][ $(this).data('translateKey') ] );
-  });
-};
-
-select.addEventListener('change', function () {
-  var selectValue = select.value;
-  $(document).ready(function(){
-      $.html5Translate(dict, selectValue);
-  });
-});
