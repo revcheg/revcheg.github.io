7043e13da0e5890a17cc2d40d515f9a70c1ccde3
Max Sharing <revcheg0@gmail.com>
2018-06-11 18:16:07 +0300

    s

---
+++ aur/dist/js/main.js
@@ -0,0 +1,30 @@
+(function() {
+
+  // глобальная переменная нашего скрипта
+  // var message = "Привет";
+
+  // функция для вывода этой переменной
+  // function showMessage() {
+    // alert( message );
+  // }
+
+  // выводим сообщение
+  // showMessage();
+
+  var main = document.querySelector('.main');
+  var closeBtn = document.querySelector('.popup__close');
+  var newsLink = document.querySelector('.nav__item--news');
+  var popup = document.querySelector('.popup');
+  
+  newsLink.addEventListener('click', function () {
+    main.style.filter = 'blur(5px)';
+    popup.style.clip = 'auto';
+    popup.style.opacity = '1';
+  });
+  
+  closeBtn.addEventListener('click', function () {
+    main.style.filter = 'blur(0px)';
+    popup.style.clip = 'rect(0, 0, 0, 0)';
+    popup.style.opacity = '0';
+  });
+})();
