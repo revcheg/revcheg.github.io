edb02aaeecb01454f952e2973a04f8834d3b3f46
Max Sharing <revcheg0@gmail.com>
2018-06-13 14:22:37 +0300

    redirect language

--- investix/js/main.js
+++ investix/js/main.js
@@ -1,15 +1,54 @@
 // Nav
-var nav = document.querySelector('.nav');
-var navToggle = document.querySelector('.header__toggle');
+(function () {
+  var nav = document.querySelector('.nav');
+  var navToggle = document.querySelector('.header__toggle');
 
-navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('header__toggle--inactive')) {
-    navToggle.classList.remove('header__toggle--inactive');
-    navToggle.classList.add('header__toggle--active');
-    nav.classList.add('nav--active');
-  } else {
-    navToggle.classList.remove('header__toggle--active');
-    navToggle.classList.add('header__toggle--inactive');
-    nav.classList.remove('nav--active');
-  }
-});
+  navToggle.addEventListener('click', function () {
+    if (navToggle.classList.contains('header__toggle--inactive')) {
+      navToggle.classList.remove('header__toggle--inactive');
+      navToggle.classList.add('header__toggle--active');
+      nav.classList.add('nav--active');
+    } else {
+      navToggle.classList.remove('header__toggle--active');
+      navToggle.classList.add('header__toggle--inactive');
+      nav.classList.remove('nav--active');
+    }
+  });
+})();
+
+// Language
+(function () {
+var select = document.querySelector('.nav__select');
+
+  // select.addEventListener('change', function () {
+  //   var selectValue = select.value;
+  // 
+  //   switch (selectValue) {
+  //     case 'en':
+  //       window.location = 'https://investix.com/index.html';
+  //       break;
+  //     case 'ru':
+  //       window.location = 'https://investix.com/ru/index.html';
+  //       break;
+  //     default:
+  //       window.location = 'https://investix.com/index.html';
+  //   }
+  // });
+  
+  // Pathname
+  select.addEventListener('change', function () {
+    var selectValue = select.value;
+    var location = window.location.pathname;
+    
+    switch (selectValue) {
+      case 'en':
+        window.location = 'https://investix.com/en' + location;
+        break;
+      case 'ru':
+        window.location = 'https://investix.com/ru' + location;
+        break;
+      default:
+        window.location = 'https://investix.com/en' + location;
+    }
+  });
+})();
