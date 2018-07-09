ebc145e6e94bc5d641a6e5fef7188343bf796dc1
Max Sharing <revcheg0@gmail.com>
2018-07-09 13:40:01 +0300

    modal

--- deep/js/main.js
+++ deep/js/main.js
@@ -14,3 +14,26 @@
     }
   });
 })();
+
+(function () {
+  var signIn = document.querySelector('.nav__signup');
+  var login = document.querySelector('.modal--login');
+  var overlay = document.querySelector('.modal__overlay');
+  var close = document.querySelector('.modal__close');
+  
+  signIn.addEventListener('click', function () {
+    login.classList.remove('modal--inactive');
+    login.classList.add('modal--active');
+    
+    overlay.classList.remove('modal__overlay--inactive');
+    overlay.classList.add('modal__overlay--active');
+  });
+  
+  close.addEventListener('click', function () {
+    login.classList.remove('modal--active');
+    login.classList.add('modal--inactive');
+    
+    overlay.classList.remove('modal__overlay--active');
+    overlay.classList.add('modal__overlay--inactive');
+  });
+})();
