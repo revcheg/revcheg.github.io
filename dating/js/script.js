f12ef7c5365cfad9fe9b88e155576ca70a9361e0
Max Sharing <revcheg0@gmail.com>
2018-07-12 15:12:09 +0300

    mobile menu

--- dating/js/script.js
+++ dating/js/script.js
@@ -132,6 +132,22 @@ jQuery(function($){
 
   });
 
-
+  // Nav
+  (function () {
+    var nav = document.querySelector('.nav__list');
+    var navToggle = document.querySelector('.nav__toggle');
+  
+    navToggle.addEventListener('click', function () {
+      if (navToggle.classList.contains('nav__toggle--inactive')) {
+        navToggle.classList.remove('nav__toggle--inactive');
+        navToggle.classList.add('nav__toggle--active');
+        nav.classList.add('nav__list--active');
+      } else {
+        navToggle.classList.remove('nav__toggle--active');
+        navToggle.classList.add('nav__toggle--inactive');
+        nav.classList.remove('nav__list--active');
+      }
+    });
+  })();
 
 
