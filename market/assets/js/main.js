043c3eba769220c427239069b13340c8f90bb5af
Max Sharing <revcheg0@gmail.com>
2018-07-13 14:39:00 +0300

    s

--- market/assets/js/main.js
+++ market/assets/js/main.js
@@ -39,3 +39,12 @@
     }
   });
 })();
+
+// Scroll
+$('.header__menu-item').click(function() {
+	var keyword = $(this).attr('ss');
+	var scrollTo = $('#' + keyword);
+	$('html, body').animate({
+		scrollTop: scrollTo.offset().top - 150
+	}, 1000);
+});
