2e5d4658ae0018864757a0fc304ef9981474f4f1
Max Sharing <revcheg0@gmail.com>
2018-10-02 17:35:55 +0300

    rebuild

--- forexglobal/gulpfile.js
+++ forexglobal/gulpfile.js
@@ -1,36 +1,45 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var livereload = require('gulp-livereload');
-var plumber = require('gulp-plumber');
-var cssmin = require('gulp-cssmin');
+var csso = require('gulp-csso');
+var htmlmin = require('gulp-htmlmin');
 var jsmin = require('gulp-jsmin');
 var rename = require('gulp-rename');
+var svgmin = require('gulp-svgmin');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', ['watch', 'main']);
+gulp.task('default', ['watch']);
 
-gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch('sass/**/*.scss', ['main']);
-  gulp.watch('js/*.js', ['main']);
+gulp.task('minifyHTML', function () {
+  gulp.src('index.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(''));
 });
 
-gulp.task('main', function () {
+gulp.task('minifySVG', function () {
+  gulp.src('app/img/svg/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/img/svg'));
+});
+
+gulp.task('styles', function () {
   gulp.src('sass/style.scss')
-    .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('css'))
-    .pipe(cssmin())
+    .pipe(csso())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('css'))
     .pipe(livereload());
-    
-  gulp.src('js/main.js')
-    .pipe(jsmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('sass/**/*.scss', ['styles']);
 });
