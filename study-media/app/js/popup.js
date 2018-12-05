b5607b075af4332a6ffe68693812a6504bcc1e1a
Max Sharing <revcheg0@gmail.com>
2018-12-05 13:01:56 +0200

    mostly finish

--- study-media/app/js/popup.js
+++ study-media/app/js/popup.js
@@ -3,7 +3,7 @@
   var popups = document.querySelectorAll('.popup');
   var closePopups = document.querySelectorAll('.popup__close');
   var overlay = document.querySelector('.popup__overlay');
-  
+
   openPopups[0].addEventListener('click', function (evt) {
     evt.preventDefault();
     popups[0].classList.add('popup--show');
@@ -13,7 +13,7 @@
       overlay.classList.remove('popup__overlay--show');
     });
   });
-  
+
   openPopups[1].addEventListener('click', function (evt) {
     evt.preventDefault();
     popups[1].classList.add('popup--show');
@@ -23,7 +23,7 @@
       overlay.classList.remove('popup__overlay--show');
     });
   });
-  
+
   openPopups[2].addEventListener('click', function (evt) {
     evt.preventDefault();
     popups[2].classList.add('popup--show');
@@ -33,7 +33,7 @@
       overlay.classList.remove('popup__overlay--show');
     });
   });
-  
+
   openPopups[3].addEventListener('click', function (evt) {
     evt.preventDefault();
     popups[3].classList.add('popup--show');
@@ -43,7 +43,7 @@
       overlay.classList.remove('popup__overlay--show');
     });
   });
-  
+
   openPopups[4].addEventListener('click', function (evt) {
     evt.preventDefault();
     popups[4].classList.add('popup--show');
