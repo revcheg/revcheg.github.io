c4c097864d3ebe60bf11f2387790e7042f7d34d2
Max Sharing <revcheg0@gmail.com>
2019-02-07 16:23:22 +0200

    var another gallery

--- study-summer/dist/js/main.js
+++ study-summer/dist/js/main.js
@@ -31,6 +31,7 @@
     popup.querySelector('.popup__webp').srcset = photos[currentPhoto].srcset;
     popup.querySelector('.popup__jpg').src = photos[currentPhoto].src;
     overlay.classList.remove('popup__overlay--hide');
+    overlay.style.backgroundImage = 'url('+ photos[currentPhoto].src +')';
   };
   
   prev.addEventListener('click', function () {
