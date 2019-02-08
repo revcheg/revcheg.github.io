ff66a68cee7f2699d1d20a2fba0f6792f5f3bbcd
Max Sharing <revcheg0@gmail.com>
2019-02-08 15:21:03 +0200

    add filter for programs

---
+++ study-summer/app/js/programs.js
@@ -0,0 +1,26 @@
+(function () {
+  var listButton = document.querySelector('.programs__button--list');
+  var tileButton = document.querySelector('.programs__button--tile');
+  var list = document.querySelector('.programs__list');
+  var item = document.querySelectorAll('.programs__item');
+  
+  listButton.addEventListener('click', function () {
+    list.classList.add('programs__list--column');
+    listButton.classList.add('programs__button--active');
+    tileButton.classList.remove('programs__button--active');
+    
+    for (var i = 0; i < item.length; i++) {
+      item[i].classList.add('programs__item--column');
+    }
+  });
+  
+  tileButton.addEventListener('click', function () {
+    list.classList.remove('programs__list--column');
+    tileButton.classList.add('programs__button--active');
+    listButton.classList.remove('programs__button--active');
+    
+    for (var i = 0; i < item.length; i++) {
+      item[i].classList.remove('programs__item--column');
+    }
+  });
+})();
--- study-summer/dist/js/main.js
+++ study-summer/dist/js/main.js
@@ -160,6 +160,32 @@
 //   };
 // })();
 (function () {
+  var listButton = document.querySelector('.programs__button--list');
+  var tileButton = document.querySelector('.programs__button--tile');
+  var list = document.querySelector('.programs__list');
+  var item = document.querySelectorAll('.programs__item');
+  
+  listButton.addEventListener('click', function () {
+    list.classList.add('programs__list--column');
+    listButton.classList.add('programs__button--active');
+    tileButton.classList.remove('programs__button--active');
+    
+    for (var i = 0; i < item.length; i++) {
+      item[i].classList.add('programs__item--column');
+    }
+  });
+  
+  tileButton.addEventListener('click', function () {
+    list.classList.remove('programs__list--column');
+    tileButton.classList.add('programs__button--active');
+    listButton.classList.remove('programs__button--active');
+    
+    for (var i = 0; i < item.length; i++) {
+      item[i].classList.remove('programs__item--column');
+    }
+  });
+})();
+(function () {
   var slides = document.querySelectorAll('.slider__item');
   var prev = document.querySelector('.slider__control--prev');
   var next = document.querySelector('.slider__control--next');
