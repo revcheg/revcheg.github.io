a6c8fc393b3d76178ae7291751221834a3b8838c
Max Sharing <revcheg0@gmail.com>
2019-02-27 14:33:51 +0200

    first step to add form

--- study-summer/dist/js/main.js
+++ study-summer/dist/js/main.js
@@ -1,4 +1,29 @@
 (function () {
+  var phone = document.querySelector('.phone');
+  var currentValue = phone.value;
+  
+  phone.addEventListener('focus', function () {
+    phone.value = '+38 (';
+  });
+  
+  phone.addEventListener('input', function () {    
+    var testValue = phone.value; 
+    var regExp = /[a-zA-Zа-яА-Я]/; 
+    if (regExp.test(testValue)) { 
+      testValue = testValue.replace(regExp, ''); 
+      phone.value = testValue; 
+    } 
+    
+    if (phone.value.length >= 8 && phone.value.length < 9) {
+      currentValue = phone.value;
+      phone.value = currentValue + ') ';
+    } else if (phone.value.length >= 13 && phone.value.length < 14) {
+      currentValue = phone.value;
+      phone.value = currentValue + '-';
+    } 
+  });
+})();    
+(function () {
   var list = document.querySelector('.leaders__list');
   var item = document.querySelectorAll('.leaders__item');
   var prev = document.querySelector('.leaders__button--prev');
