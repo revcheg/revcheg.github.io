8b1d1b430c95e05ecd8a9c80499794c288d469d5
Max Sharing <revcheg0@gmail.com>
2019-03-11 16:37:35 +0200

    new country

---
+++ study-summer/canada/app/js/programs-data.js
@@ -0,0 +1,82 @@
+var programsData = [
+  {
+    full: false,
+    webp: 'img/program-1.webp',
+    jpg: 'img/program-1.jpg',
+    alt: 'University of Toronto',
+    name: 'University of Toronto',
+    location: 'Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/39" target="_blank">22.06 - 13.07 (3 нед.)</a><br><a href="http://summer.study.ua/trip/look/51" target="_blank">20.07 - 10.08 (3 нед.)</a>',
+    cost: '$5500 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-2.webp',
+    jpg: 'img/program-2.jpg',
+    alt: 'University of Toronto Advanced Academic Program',
+    name: 'University of Toronto Advanced Academic Program',
+    location: 'Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/1133" target="_blank">27.07 - 24.08 (4 нед.)</a>',
+    cost: '$6200 (4 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-3.webp',
+    jpg: 'img/program-3.jpg',
+    alt: 'University of Toronto + McGill University',
+    name: 'University of Toronto + McGill University',
+    location: 'Торонто + Монреаль',
+    date: '<a href="http://summer.study.ua/trip/look/63" target="_blank">13.07 - 10.08 (4 нед.)</a>',
+    cost: '$6500 (4 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-4.webp',
+    jpg: 'img/program-4.jpg',
+    alt: 'Braemar College',
+    name: 'Braemar College',
+    location: 'Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/27" target="_blank">1.07 - 27.07 (4 нед.)</a><br><a>1.07 - 17.08 (7 нед.)</a>',
+    cost: '$5100 (4 нед.)<br>$7600 (7 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-5.webp',
+    jpg: 'img/program-5.jpg',
+    alt: 'Ryerson University',
+    name: 'Ryerson University',
+    location: 'Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/1118" target="_blank">29.06 - 20.07 (3 нед.)</a>',
+    cost: '$5600 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-6.webp',
+    jpg: 'img/program-6.jpg',
+    alt: 'Sheridan College',
+    name: 'Sheridan College',
+    location: 'Оуквилль',
+    date: '<a href="http://summer.study.ua/trip/look/1134" target="_blank">1.07 — 22.07 (3 нед.)</a>',
+    cost: '$5300 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-7.webp',
+    jpg: 'img/program-7.jpg',
+    alt: 'ILAC',
+    name: 'ILAC',
+    location: 'Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/7" target="_blank">6.07 - 27.07 (3 нед.)</a><br><a href="http://summer.study.ua/trip/look/1129" target="_blank">3.08 - 24.08 (3 нед.)</a>',
+    cost: '$4800 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-8.webp',
+    jpg: 'img/program-8.jpg',
+    alt: 'University of British Columbia',
+    name: 'University of British Columbia',
+    location: 'Ванкувер',
+    date: '<a href="http://summer.study.ua/trip/look/1117" target="_blank">7.07 - 28.07 (3 нед.)</a>',
+    cost: '$5800 (3 нед.)'
+  }
+];
