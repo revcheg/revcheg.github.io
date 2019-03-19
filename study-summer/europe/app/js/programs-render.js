d5aed465de10bd8347aa39ce181a40692d344243
Max Sharing <revcheg0@gmail.com>
2019-03-19 17:07:14 +0200

    add europe

---
+++ study-summer/europe/app/js/programs-render.js
@@ -0,0 +1,63 @@
+(function () {
+  var list = document.querySelector('.programs__list');
+  var load = document.querySelector('.programs__load');
+  var listButton = document.querySelector('.programs__button--list');
+  var programsTemplate = document.querySelector('.programs__template').content;
+  
+  if (document.body.clientWidth > 1210) {
+    programsCounter = 3;
+    programsIncrement = 3;
+  } else if (document.body.clientWidth < 1210) {
+    programsCounter = 2;
+    programsIncrement = 2;
+  }
+  
+  renderPrograms();
+  
+  load.addEventListener('click', function () {
+    programsCounter = programsCounter + programsIncrement;
+    clearPrograms();
+    renderPrograms();
+  });
+  
+  function clearPrograms() {
+    var item = document.querySelectorAll('.programs__item');
+    for (var j = 0; j < item.length; j++) {
+      item[j].remove();
+    }
+  };
+  
+  function renderPrograms() {
+    var programsLength = programsData.slice(0, programsCounter).length;
+    
+    for (var i = 0; i < programsLength; i++) {
+      var item = document.createElement('li');
+      item.classList.add('programs__item');
+      
+      if (listButton.classList.contains('programs__button--active')) {
+        item.classList.add('programs__item--column');
+      } 
+      
+      var content = programsTemplate.cloneNode(true);
+      if (programsData[i].full) {
+        content.querySelector('.programs__full').style.display = 'block';
+      }
+      content.querySelector('.programs__flag-webp').srcset = programsData[i].flagWebp;
+      content.querySelector('.programs__flag').src = programsData[i].flag;
+      content.querySelector('.programs__flag').alt = programsData[i].flagAlt;
+      content.querySelector('.programs__img-webp').srcset = programsData[i].webp;
+      content.querySelector('.programs__img').src = programsData[i].jpg;
+      content.querySelector('.programs__img').alt = programsData[i].alt;
+      content.querySelector('.programs__name').innerHTML = programsData[i].name;
+      content.querySelector('.programs__location').innerHTML = programsData[i].location;
+      content.querySelector('.programs__date').innerHTML = programsData[i].date;
+      content.querySelector('.programs__cost').innerHTML = programsData[i].cost;
+      item.appendChild(content);
+      list.appendChild(item);
+    }
+    
+    if (programsCounter >= programsData.length) {
+      load.disabled = true;
+    }
+  };
+})();
