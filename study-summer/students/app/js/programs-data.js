cfd3a902625c11a1d0f47466efe24ef6bbfb8ecd
Max Sharing <revcheg0@gmail.com>
2019-03-20 13:19:21 +0200

    add new students page

---
+++ study-summer/students/app/js/programs-data.js
@@ -0,0 +1,132 @@
+var programsData = [
+  {
+    full: false,
+    webp: 'img/program-1.webp',
+    jpg: 'img/program-1.jpg',
+    alt: 'EC English, London',
+    name: 'EC English, London',
+    location: 'Великобритания, Лондон',
+    date: '<a href="http://summer.study.ua/trip/look/1125" target="_blank">20.07 - 03.08 (2 нед.)</a>',
+    cost: '$3550 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-2.webp',
+    jpg: 'img/program-2.jpg',
+    alt: 'EC English, London + Dublin experience',
+    name: 'EC English, London + Dublin experience',
+    location: 'Великобритания, Лондон. Ирландия, Дублин',
+    date: '<a href="http://summer.study.ua/trip/look/1126" target="_blank">20.07 - 10.08 (3 нед.)</a><br><a href="http://summer.study.ua/trip/look/1127" target="_blank">20.07 - 17.08 (3 нед.)</a>',
+    cost: '$4600 (3 нед.)<br>$5300 (4 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-3.webp',
+    jpg: 'img/program-3.jpg',
+    alt: 'Malta Summer',
+    name: 'Malta Summer',
+    location: 'Мальта, Сент-Джулианс',
+    date: '<a href="http://summer.study.ua/trip/look/1124" target="_blank">20.07 - 03.08 (2 нед.)</a>',
+    cost: '€2450 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-4.webp',
+    jpg: 'img/program-4.jpg',
+    alt: 'EU Business School',
+    name: 'EU Business School',
+    location: 'Испания, Барселона',
+    date: '<a href="http://summer.study.ua/trip/look/1099" target="_blank">11.08 - 25.08 (2 нед.)</a>',
+    cost: '€4100 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-5.webp',
+    jpg: 'img/program-5.jpg',
+    alt: 'Barcelona Spanish',
+    name: 'Barcelona Spanish',
+    location: 'Испания, Барселона',
+    date: '<a>06.07 - 20.07 (2 нед.)</a>',
+    cost: '€3100 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-6.webp',
+    jpg: 'img/program-6.jpg',
+    alt: 'Barcelona English',
+    name: 'Barcelona English',
+    location: 'Испания, Барселона',
+    date: '<a>06.07 - 20.07 (2 нед.)</a>',
+    cost: '€3300 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-7.webp',
+    jpg: 'img/program-7.jpg',
+    alt: 'Oxford School of English',
+    name: 'Oxford School of English',
+    location: 'Великобритания, Оксфорд',
+    date: '<a href="http://summer.study.ua/trip/look/69" target="_blank">27.07 - 18.08 (3 нед.)</a>',
+    cost: '$4100 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-8.webp',
+    jpg: 'img/program-8.jpg',
+    alt: 'ILAC',
+    name: 'ILAC',
+    location: 'Канада, Торонто',
+    date: '<a href="http://summer.study.ua/trip/look/73" target="_blank">27.07 - 24.08 (4 нед.)</a>',
+    cost: '$5100 (4 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-9.webp',
+    jpg: 'img/program-9.jpg',
+    alt: 'EC English Toronto + Vancouver',
+    name: 'EC English Toronto + Vancouver',
+    location: 'Канада, Торонто, Ванкувер',
+    date: '<a href="http://summer.study.ua/trip/look/73" target="_blank">27.07 - 24.08 (4 нед.)</a>',
+    cost: '$5700 (резиденция)<br>$4750 (семья)'
+  },
+  {
+    full: false,
+    webp: 'img/program-10.webp',
+    jpg: 'img/program-10.jpg',
+    alt: 'Niagara College',
+    name: 'Niagara College',
+    location: 'Канада, Велланд',
+    date: '<a href="http://summer.study.ua/trip/look/42" target="_blank">27.07 - 24.08 (4 нед.)</a>',
+    cost: '$4700 (4 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-11.webp',
+    jpg: 'img/program-11.jpg',
+    alt: 'EC English New York',
+    name: 'EC English New York',
+    location: 'США, Нью-Йорк',
+    date: '<a href="http://summer.study.ua/trip/look/1128" target="_blank">13.07 - 27.08 (2 нед.)</a>',
+    cost: '$4700 (2 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-12.webp',
+    jpg: 'img/program-12.jpg',
+    alt: 'EC English New York + Miami',
+    name: 'EC English New York + Miami',
+    location: 'США,  Нью-Йорк, Майами',
+    date: '<a href="http://summer.study.ua/trip/look/1137" target="_blank">13.07 - 03.08 (3 нед.)</a>',
+    cost: '$5700 (3 нед.)'
+  },
+  {
+    full: false,
+    webp: 'img/program-13.webp',
+    jpg: 'img/program-13.jpg',
+    alt: 'Hotel and Restaurant business experience',
+    name: 'Hotel and Restaurant business experience',
+    location: 'Швейцария, Люцерн',
+    date: '<a href="http://summer.study.ua/trip/look/61" target="_blank">06.07 - 19.07 (2 нед.)</a>',
+    cost: '$4500 (2 нед.)'
+  }
+];
