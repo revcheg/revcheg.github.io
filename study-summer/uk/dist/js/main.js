115f6135ae37b70fc5f151ed2beabc5ac6f6be48
Max Sharing <revcheg0@gmail.com>
2019-03-21 16:15:14 +0200

    change form

--- study-summer/uk/dist/js/main.js
+++ study-summer/uk/dist/js/main.js
@@ -1,38 +1,38 @@
-(function () {
-  var phone = document.querySelector('.request__input--phone');
-  var currentValue = phone.value;
-  
-  // Custom mask
-  phone.addEventListener('focus', function () {
-    phone.value = '+38 (';
-  });
-  
-  phone.addEventListener('input', function () {    
-    var testValue = phone.value; 
-    var regExp = /[a-zA-Zа-яА-Я]/; 
-    if (regExp.test(testValue)) { 
-      testValue = testValue.replace(regExp, ''); 
-      phone.value = testValue; 
-    } 
-    
-    if (phone.value.length >= 8 && phone.value.length < 9) {
-      currentValue = phone.value;
-      phone.value = currentValue + ') ';
-    } else if (phone.value.length >= 13 && phone.value.length < 14) {
-      currentValue = phone.value;
-      phone.value = currentValue + '-';
-    } 
-  });
-  
-  // Decorate
-  var inputs = document.querySelectorAll('.request__input');
-  for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('focus', function () {
-      var label = this.nextElementSibling;
-      label.classList.add('request__label--active');
-    });
-  }
-})();    
+// (function () {
+//   var phone = document.querySelector('.request__input--phone');
+//   var currentValue = phone.value;
+// 
+//   // Custom mask
+//   phone.addEventListener('focus', function () {
+//     phone.value = '+38 (';
+//   });
+// 
+//   phone.addEventListener('input', function () {    
+//     var testValue = phone.value; 
+//     var regExp = /[a-zA-Zа-яА-Я]/; 
+//     if (regExp.test(testValue)) { 
+//       testValue = testValue.replace(regExp, ''); 
+//       phone.value = testValue; 
+//     } 
+// 
+//     if (phone.value.length >= 8 && phone.value.length < 9) {
+//       currentValue = phone.value;
+//       phone.value = currentValue + ') ';
+//     } else if (phone.value.length >= 13 && phone.value.length < 14) {
+//       currentValue = phone.value;
+//       phone.value = currentValue + '-';
+//     } 
+//   });
+// 
+//   // Decorate
+//   var inputs = document.querySelectorAll('.request__input');
+//   for (var i = 0; i < inputs.length; i++) {
+//     inputs[i].addEventListener('focus', function () {
+//       var label = this.nextElementSibling;
+//       label.classList.add('request__label--active');
+//     });
+//   }
+// })();    
 (function () {
   var list = document.querySelector('.leaders__list');
   var item = document.querySelectorAll('.leaders__item');
