a63e09e02f56a4295bba1c609b7497b18c8b8f6f
Max Sharing <revcheg0@gmail.com>
2019-04-02 16:36:37 +0300

    s

--- study-summer/canada/app/js/add.js
+++ study-summer/canada/app/js/add.js
@@ -7,24 +7,24 @@
   window.onload = function () {
     add.classList.add('add--show');
     overlay.classList.remove('popup__overlay--hide');
-    document.body.style.overflow = 'hidden';
+    // document.body.style.overflow = 'hidden';
   };
   
   close.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
   
   overlay.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
   
   link.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
 })();
--- study-summer/canada/dist/js/main.js
+++ study-summer/canada/dist/js/main.js
@@ -7,25 +7,25 @@
   window.onload = function () {
     add.classList.add('add--show');
     overlay.classList.remove('popup__overlay--hide');
-    document.body.style.overflow = 'hidden';
+    // document.body.style.overflow = 'hidden';
   };
   
   close.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
   
   overlay.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
   
   link.addEventListener('click', function () {
     add.classList.remove('add--show');
     overlay.classList.add('popup__overlay--hide');
-    document.body.style.overflow = 'auto';
+    // document.body.style.overflow = 'auto';
   });
 })();
 // (function () {
