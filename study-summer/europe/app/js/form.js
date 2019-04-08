37a3ecb92d2b9dc7e62fc22e18534a32d39a4f79
Max Sharing <revcheg0@gmail.com>
2019-04-08 15:55:51 +0300

    new form

--- study-summer/europe/app/js/form.js
+++ study-summer/europe/app/js/form.js
@@ -1,34 +1,41 @@
-// (function () {
-//   var phone = document.querySelector('.request__input--phone');
-//   var currentValue = phone.value;
-// 
-//   phone.addEventListener('focus', function () {
-//     phone.value = '+38 (';
-//   });
-// 
-//   phone.addEventListener('input', function () {    
-//     var testValue = phone.value; 
-//     var regExp = /[a-zA-Zа-яА-Я]/; 
-//     if (regExp.test(testValue)) { 
-//       testValue = testValue.replace(regExp, ''); 
-//       phone.value = testValue; 
-//     } 
-// 
-//     if (phone.value.length >= 8 && phone.value.length < 9) {
-//       currentValue = phone.value;
-//       phone.value = currentValue + ') ';
-//     } else if (phone.value.length >= 13 && phone.value.length < 14) {
-//       currentValue = phone.value;
-//       phone.value = currentValue + '-';
-//     } 
-//   });
-// 
-//   var inputs = document.querySelectorAll('.request__input');
-// 
-//   for (var i = 0; i < inputs.length; i++) {
-//     inputs[i].addEventListener('focus', function () {
-//       var label = this.nextElementSibling;
-//       label.classList.add('request__label--active');
-//     });
-//   }
-// })();    
+(function () {
+  var phone = document.querySelector('.request__input[type=tel]');
+
+  phone.addEventListener('focus', function () {
+    phone.value = '+380';
+    var label = this.nextElementSibling;
+    label.classList.add('request__label--active');
+  });
+  
+  phone.addEventListener('input', function () {    
+    var currentValue = phone.value; 
+    var regExp = /[^\d+]/; 
+    if (regExp.test(currentValue)) { 
+      currentValue = currentValue.replace(regExp, ''); 
+      phone.value = currentValue; 
+    } 
+  });
+  
+  var inputs = document.querySelectorAll('.request__input');
+
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', function () {
+      if (this.checkValidity()) {
+        this.classList.remove('request__input--invalid');
+        this.classList.add('request__input--valid');
+      } else {
+        this.classList.add('request__input--invalid');
+        this.classList.remove('request__input--valid');
+      }
+      
+      var label = this.nextElementSibling;
+      var inputValue = this.value;
+      if (inputValue == '') {
+        label.classList.remove('request__label--active');
+        this.classList.remove('request__input--invalid');
+      } else {
+        label.classList.add('request__label--active');
+      }
+    });
+  }
+})();    
