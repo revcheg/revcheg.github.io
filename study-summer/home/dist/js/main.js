96d5ab5be82ee3ecfff685d08d2da15520650ee2
Max Sharing <revcheg0@gmail.com>
2019-05-17 14:52:12 +0300

    sync all scroll, nav, link element between all of them

--- study-summer/home/dist/js/main.js
+++ study-summer/home/dist/js/main.js
@@ -40,6 +40,46 @@
   }
 })();
 (function () {
+  var anchors = document.querySelectorAll('.header__link');
+  var home = document.querySelector('.header__home');
+  
+  var indexButton = 0;
+  var anchorsArray = [];
+  for (var i = 0; i < anchors.length; i++){
+    anchorsArray.push(anchors[i]);
+    anchors[i].addEventListener('click', function(evt){
+      evt.preventDefault();
+      indexButton = anchorsArray.indexOf(evt.currentTarget);
+      generateIndex();
+    });
+  }
+  
+  function generateIndex () {
+    switch (indexButton) {
+      case 0:
+        scrollCounter = 1;
+        break;
+      case 1:
+        scrollCounter = 6;
+        break;
+      case 2:
+        scrollCounter = 7;
+        break;
+      case 3:
+        scrollCounter = 9;
+        break;
+      default:
+        scrollCounter = 0;
+    }
+    scrolling();
+  };
+  
+  home.addEventListener('click', function () {
+    scrollCounter = 0;
+    scrolling();
+  });
+})();
+(function () {
   var anchors = document.querySelectorAll('.nav__anchor');
   
   var indexButton = 0;
