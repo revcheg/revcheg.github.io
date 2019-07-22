ad184150e3090b75d735b1e2428e5834bb475549
Max Sharing <revcheg0@gmail.com>
2019-07-22 13:48:10 +0300

    ssss

--- study-summer/uk/app/js/program/tabs.js
+++ study-summer/uk/app/js/program/tabs.js
@@ -1,35 +1,4 @@
 (function () {
-  
-})();
-(function () {
-  var button = document.querySelector('.nav__menu');
-  var nav = document.querySelector('.nav__list');
-  var links = document.querySelectorAll('.nav__link');
-
-  button.addEventListener('click', function () {
-    if (button.classList.contains('nav__menu--open')) {
-      nav.classList.add('nav__list--show');
-      button.classList.remove('nav__menu--open');
-      button.classList.add('nav__menu--close');
-      document.body.style.overflow = 'hidden';
-    } else {
-      nav.classList.remove('nav__list--show');
-      button.classList.remove('nav__menu--close');
-      button.classList.add('nav__menu--open');
-      document.body.style.overflow = 'auto';
-    }
-  });
-  
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', function () {
-      nav.classList.remove('nav__list--show');
-      button.classList.remove('nav__menu--close');
-      button.classList.add('nav__menu--open');
-      document.body.style.overflow = 'auto';
-    });
-  }
-})();
-(function () {
   var tabButtons = document.querySelectorAll('.info__button');
   var tabSections = document.querySelectorAll('.info__tab');
   var tempArray = [];
@@ -74,16 +43,4 @@
       galleryList.style.left = '-' + counter + '00%';
     });
   };
-})();
-
-(function () {
-  var button = document.querySelector('.intro__play');
-  var column = document.querySelector('.intro__column');
-  var video = document.querySelector('.intro__video');
-  
-  button.addEventListener('click', function () {
-    this.style.display = 'none';
-    column.style.width = '100%';
-    video.src = 'https://www.youtube.com/embed/LpO92XC8050?&autoplay=1';
-  });
 })();
