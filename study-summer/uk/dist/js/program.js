c7a7a2d89ff6754d0e27fbea8e051637c640601c
Max Sharing <revcheg0@gmail.com>
2019-07-23 13:02:54 +0300

    s

--- study-summer/uk/dist/js/program.js
+++ study-summer/uk/dist/js/program.js
@@ -2,6 +2,29 @@
   
 })();
 (function () {
+  var list = document.querySelector('.leader__list');
+  var numberOfSlide = list.querySelectorAll('li');
+  var prev = document.querySelector('.leader__button--prev');
+  var next = document.querySelector('.leader__button--next');
+  var counter = 0;
+  
+  next.addEventListener('click', function () {
+    if (counter >= numberOfSlide.length - 2) {
+      counter = numberOfSlide.length - 2;
+    }
+    counter++;
+    list.style.left = '-' + counter + '00%';
+  });
+  
+  prev.addEventListener('click', function () {
+    counter--;
+    if (counter <= 0) {
+      counter = 0;
+    }
+    list.style.left = '-' + counter + '00%';
+  });
+})();
+(function () {
   var button = document.querySelector('.nav__menu');
   var nav = document.querySelector('.nav__list');
   var links = document.querySelectorAll('.nav__link');
