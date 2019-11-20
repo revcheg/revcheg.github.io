83c81043b4c5074404862c6a7dbc3be5e439a5aa
Max Sharing <revcheg0@gmail.com>
2019-11-20 15:28:48 +0200

    add

--- study-summer/event/build/js/main.js
+++ study-summer/event/build/js/main.js
@@ -8,6 +8,34 @@
   });
 })();
 (function () {
+  let slider = document.querySelector('.leader__list');
+  let prev = document.querySelector('.leader__button--prev');
+  let next = document.querySelector('.leader__button--next');
+  let counter = 0;
+  
+  prev.addEventListener('click', prevSlide);
+  function prevSlide() {
+    counter--;
+    if (counter < 0) {
+      counter = 3;
+    }
+    moveSlider();
+  };
+  
+  next.addEventListener('click', nextSlide);
+  function nextSlide() {
+    counter++;
+    if (counter > 3) {
+      counter = 0;
+    }
+    moveSlider();
+  };
+  
+  function moveSlider() {
+    slider.style.left = '-' + counter + '00%';
+  };
+})();
+(function () {
   var phone = document.querySelector('.request__input[type=tel]');
   
   phone.addEventListener('focus', function() {
