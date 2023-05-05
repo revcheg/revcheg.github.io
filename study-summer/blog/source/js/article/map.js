66a7a62d9e30c20136e8de8f89df7f60d90fa223
Max Sharing <revcheg0@gmail.com>
2023-05-05 18:38:46 +0300

    Почистив сміття

--- study-summer/blog/source/js/article/map.js
+++
@@ -1,7 +0,0 @@
-// (function () {
-//   var iframe = document.querySelector('.short__map');
-// 
-//   iframe.addEventListener('mouseover', function () {
-//     this.src = 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1088.4094561287006!2d-79.90590150505048!3d43.25761206235831!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882c9b5ad285329d%3A0x6433b0053afef018!2z0JrQvtC70LvQtdC00LYg0JrQvtC70YPQvNCx0LjRjyDQmNC90YLQtdGA0L3QtdGI0L3QtdC7!5e0!3m2!1sru!2sua!4v1567765101160!5m2!1sru!2sua';
-//   }, {once: true});
-// })();
--- study-summer/blog/source/js/video.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var video = document.querySelector('.about__iframe');
-  var button = document.querySelector('.about__open');
-  
-  button.addEventListener('click', function () {
-    button.style.display = 'none';
-    video.src = 'https://www.youtube.com/embed/d4FKC4Bg2hc?&autoplay=1';
-  });
-})();
