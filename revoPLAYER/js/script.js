b9a68aae71a8113bffe08e7a7e9593cba3989486
Max Sharing <revcheg0@gmail.com>
2023-06-05 21:15:55 +0300

    Додав більше обробників помилок, для візуального фідбеку ситуації при несправностях

--- revoPLAYER/js/script.js
+++ revoPLAYER/js/script.js
@@ -1,7 +1,7 @@
-const BODY = document.querySelector('.body'); 
+const BODY = document.querySelector('.body');
 
-const VIDEO = document.querySelector('.video'); 
-const WRAPPER = document.querySelector('.video__wrapper'); 
+const VIDEO = document.querySelector('.video');
+const WRAPPER = document.querySelector('.video__wrapper');
 const SETTINGS = document.querySelector('.settings');
 const STATISTICS = document.querySelector('.statistics');
 
@@ -65,20 +65,20 @@ const muteButton = CONTROLS.querySelector('.control__button--mute');
 const muteButtonIcon = CONTROLS.querySelector('.control__mute');
 
 function muteVideo() {
-  let currentVolume = VIDEO.volume;
+  let savedVolume = VIDEO.volume;
 
   if (VIDEO.muted) {
     VIDEO.muted = false;
-    VIDEO.volume = currentVolume;
+    VIDEO.volume = savedVolume;
     volumeRange.value = VIDEO.volume;
-    
-    if (currentVolume <= 0) {
+
+    if (savedVolume <= 0) {
       VIDEO.volume = 0.5;
       volumeRange.value = 0.5;
     }
   } else {
     VIDEO.muted = true;
-    volumeRange.value = '0';
+    volumeRange.value = 0;
   }
 }
 
@@ -99,10 +99,10 @@ VIDEO.volume = 0.5;
 const volumeRange = CONTROLS.querySelector('.control__range--volume');
 
 function changeVolume(amount) {
-  let newVolume = VIDEO.volume + amount;
-  newVolume = Math.max(0, Math.min(1, newVolume));
-  VIDEO.volume = newVolume;
-  volumeRange.value = newVolume;
+  let changedVolume = VIDEO.volume + amount;
+  changedVolume = Math.max(0, Math.min(1, changedVolume));
+  VIDEO.volume = changedVolume;
+  volumeRange.value = changedVolume;
   updateVolume();
 }
 
@@ -120,15 +120,6 @@ function updateVolume() {
 
 volumeRange.addEventListener('input', updateVolume);
 
-// Extra line
-let lineProgress;
-
-function extraLine() {
-  lineProgress = (videoCurrentTime / videoDuration) * 100;
-  line.style.width = lineProgress + '%';
-  line.value = lineProgress;
-}
-
 // Range
 let rangeValue;
 
@@ -142,8 +133,7 @@ VIDEORANGE.addEventListener('mousedown', () => {
 });
 
 VIDEORANGE.addEventListener('change', function () {
-  rangeValue = VIDEORANGE.value;
-  VIDEO.currentTime = rangeValue;
+  VIDEO.currentTime = VIDEORANGE.value;
   VIDEO.play();
 
   if (pauseButtonIcon.classList.contains('control__icon--hide')) {
@@ -151,42 +141,92 @@ VIDEORANGE.addEventListener('change', function () {
   }
 });
 
+VIDEORANGE.addEventListener('input', function () {
+  let inputDuration = VIDEORANGE.value;
+  VIDEO.currentTime = inputDuration;
+
+  currentVideoPassed = formatTime(inputDuration);
+  currentVideoLeft = formatTime(videoDuration - inputDuration);
+
+  videoPassed.innerHTML = currentVideoPassed;
+  videoLeft.innerHTML = currentVideoLeft;
+});
+
 // Full screen
 const fullButton = CONTROLS.querySelector('.control__button--size');
-
-// function fullscreenVideo() {
-//   VIDEO.requestFullscreen();
-// }
+const fullButtonSize = CONTROLS.querySelector('.control__icon--size');
+const fullButtonMin = CONTROLS.querySelector('.control__icon--min');
 
 function openFullscreen() {
-  if (VIDEO.requestFullscreen) {
-    VIDEO.requestFullscreen();
-  } else if (VIDEO.mozRequestFullScreen) {
-    VIDEO.mozRequestFullScreen();
-  } else if (VIDEO.webkitRequestFullscreen) {
-    VIDEO.webkitRequestFullscreen();
-  } else if (VIDEO.msRequestFullscreen) {
-    VIDEO.msRequestFullscreen();
+  if (WRAPPER.requestFullscreen) {
+    WRAPPER.requestFullscreen();
+  } else if (WRAPPER.mozRequestFullScreen) {
+    WRAPPER.mozRequestFullScreen();
   }
 }
 
-// function setupFullscreen() {
-//   if (document.fullscreenElement) {
-//     CONTROLS.classList.remove('control--hide');
-//   } else {
-//     CONTROLS.classList.add('control--hide');
-//   }
-// }
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  }
+}
 
-fullButton.addEventListener('click', openFullscreen);
-// BODY.addEventListener('fullscreenchange', setupFullscreen);
+function changeFullscreen() {
+  if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement) {
+    exitFullscreen();
+    fullButtonSize.classList.toggle('control__icon--hide');
+    fullButtonMin.classList.toggle('control__icon--hide');
+  } else {
+    openFullscreen();
+    fullButton.setAttribute('aria-label', 'Закрити повний екран');
+    fullButton.setAttribute('title', 'Закрити повний екран (esc)');
+    fullButtonSize.classList.toggle('control__icon--hide');
+    fullButtonMin.classList.toggle('control__icon--hide');
+  }
+}
+
+fullButton.addEventListener('click', changeFullscreen);
+VIDEO.addEventListener('dblclick', changeFullscreen);
+
+// Mouse
+let mouseStoppedTimer;
+
+function handleMouseMove(event) {
+  clearTimeout(mouseStoppedTimer);
+
+  const isMouseOnControls = event.target === CONTROLS || CONTROLS.contains(event.target);
+
+  if (!isMouseOnControls) {
+    showControls();
+
+    mouseStoppedTimer = setTimeout(() => {
+      hideControls();
+    }, 3000);
+  }
+};
+
+function showControls() {
+  CONTROLS.style.transform = 'translateY(0)';
+  STATISTICS.style.transform = 'translateY(0)';
+  statisticsUFH.style.transform = 'translateY(0)';
+  VIDEO.style.cursor = 'auto';
+};
+
+function hideControls() {
+  CONTROLS.style.transform = 'translateY(100%)';
+  STATISTICS.style.transform = 'translateY(-100%)';
+  statisticsUFH.style.transform = 'translateY(-150%)';
+  VIDEO.style.cursor = 'none';
+};
+
+WRAPPER.addEventListener("mousemove", handleMouseMove);
 
 // Error
-const errorText = ERROR.querySelector('.error__text');
-// const errorCode = ERROR.querySelector('.error__code');
-
-function showError() {
+function showError(errorMessage) {
   ERROR.classList.remove('error--hide');
+  ERROR.innerHTML = errorMessage;
 
   setTimeout(() => {
     ERROR.classList.add('error--hide');
@@ -237,6 +277,108 @@ function isSupportedFileType(fileType) {
   return supportedFormats.includes(fileType);
 }
 
+// Autoplay video list
+const prevButton = CONTROLS.querySelector('.control__button--prev');
+const nextButton = CONTROLS.querySelector('.control__button--next');
+
+let currentCategory = 'TheWitcher';
+let currentSubcategory = 'deep';
+let currentVideoIndex = 0;
+let data = null;
+
+fetch('videos.json')
+  .then(response => {
+    if (!response.ok) {
+      showError('Помилка загрузки json &#128531;');
+      throw new Error('Failed to load videos.json');
+    }
+    return response.json();
+  })
+  .then(videoData => {
+    data = videoData;
+    currentCategory = 'TheWitcher';
+    currentSubcategory = 'deep';
+    currentVideoIndex = 0;
+
+    playCurrentVideo();
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });
+
+function playCurrentVideo() {
+  const currentVideo = data[currentCategory][currentSubcategory][currentVideoIndex];
+  // const videoTitle = currentVideo.title;
+  const videoUrl = currentVideo.url;
+  const videoDescription = currentVideo.description;
+
+  // VIDEO.setAttribute('title', videoTitle);
+  VIDEO.setAttribute('src', videoUrl);
+  VIDEO.setAttribute('alt', videoDescription);
+}
+
+function nextVideo() {
+  resetVideo();
+  currentVideoIndex++;
+  if (currentVideoIndex >= data[currentCategory][currentSubcategory].length) {
+    const subcategories = Object.keys(data[currentCategory]);
+    const currentSubcategoryIndex = subcategories.indexOf(currentSubcategory);
+
+    if (currentSubcategoryIndex < subcategories.length - 1) {
+      currentSubcategory = subcategories[currentSubcategoryIndex + 1];
+      currentVideoIndex = 0;
+    } else {
+      const categories = Object.keys(data);
+      const currentCategoryIndex = categories.indexOf(currentCategory);
+
+      if (currentCategoryIndex < categories.length - 1) {
+        currentCategory = categories[currentCategoryIndex + 1];
+        currentSubcategory = Object.keys(data[currentCategory])[0];
+        currentVideoIndex = 0;
+      } else {
+        currentCategory = categories[0];
+        currentSubcategory = Object.keys(data[currentCategory])[0];
+        currentVideoIndex = 0;
+      }
+    }
+  }
+  playCurrentVideo();
+}
+
+function previousVideo() {
+  resetVideo();
+  if (currentVideoIndex > 0) {
+    currentVideoIndex--;
+  } else {
+    const subcategories = Object.keys(data[currentCategory]);
+    const currentSubcategoryIndex = subcategories.indexOf(currentSubcategory);
+
+    if (currentSubcategoryIndex > 0) {
+      currentSubcategory = subcategories[currentSubcategoryIndex - 1];
+      currentVideoIndex = data[currentCategory][currentSubcategory].length - 1;
+    } else {
+      const categories = Object.keys(data);
+      const currentCategoryIndex = categories.indexOf(currentCategory);
+
+      if (currentCategoryIndex > 0) {
+        currentCategory = categories[currentCategoryIndex - 1];
+        const previousSubcategory = Object.keys(data[currentCategory]).pop();
+        currentSubcategory = previousSubcategory;
+        currentVideoIndex = data[currentCategory][previousSubcategory].length - 1;
+      } else {
+        currentCategory = categories[categories.length - 1];
+        const previousSubcategory = Object.keys(data[currentCategory]).pop();
+        currentSubcategory = previousSubcategory;
+        currentVideoIndex = data[currentCategory][previousSubcategory].length - 1;
+      }
+    }
+  }
+  playCurrentVideo();
+}
+
+prevButton.addEventListener('click', previousVideo);
+nextButton.addEventListener('click', nextVideo);
+
 // Keyboard
 let videoKey;
 
@@ -272,7 +414,15 @@ VIDEO.addEventListener('keyup', (event) => {
       break;
 
     case 'f':
-      fullscreenVideo();
+      changeFullscreen();
+      break;
+
+    case ',':
+      previousVideo();
+      break;
+
+    case '.':
+      nextVideo();
       break;
   }
 });
@@ -500,10 +650,12 @@ function resetVideo() {
   STARTBUTTON.classList.remove('video__start--hide');
   CONTROLS.classList.add('control--hide');
   STATISTICS.classList.add('statistics--hide');
+  statisticsUFH.classList.add('statistics--off');
 
   playButtonIcon.classList.add('control__icon--hide');
   pauseButtonIcon.classList.remove('control__icon--hide');
 
+  stopProgress();
   resetDuration();
 }
 
@@ -576,36 +728,91 @@ deepCheckbox.addEventListener('change', function (event) {
 // });
 
 // Extra line
+let lineProgress;
+
 const line = CONTROLS.querySelector('.control__line');
 const lineCheckbox = SETTINGS.querySelector('.settings__checkbox--line');
 
-lineCheckbox.addEventListener('change', function (event) {
-  if (event.currentTarget.checked) {
+function showExtraLine() {
+  if (lineCheckbox.checked) {
     line.classList.remove('control__line--hide');
   } else {
     line.classList.add('control__line--hide');
   }
-});
+}
+
+function extraLine() {
+  lineProgress = Math.round((videoCurrentTime / videoDuration) * 100);
+  line.style.width = lineProgress + '%';
+  line.value = lineProgress;
+}
+
+lineCheckbox.addEventListener('change', showExtraLine);
 
 // 3D scale
 const scaleCheckbox = SETTINGS.querySelector('.settings__checkbox--scale');
 
-// Start 
+function setupScale() {
+  if (scaleCheckbox.checked) {
+    BODY.addEventListener('mousemove', setScale);
+  } else {
+    WRAPPER.removeAttribute('style');
+    BODY.removeEventListener('mousemove', setScale);
+  }
+}
+
+function setScale(event) {
+  if (scaleCheckbox.checked && event.isTrusted) {
+    let xPos = -(event.pageX / window.innerWidth - 0.5) * -20;
+    let yPos = (event.pageY / window.innerHeight - 0.5) * -20;
+    let blockRect = VIDEO.getBoundingClientRect();
+    let mouseX = event.clientX - blockRect.left;
+    let mouseY = event.clientY - blockRect.top;
+
+    if (!(mouseX >= 0 && mouseX < blockRect.width && mouseY >= 0 && mouseY < blockRect.height)) {
+      WRAPPER.style.transform = 'perspective(1000px) rotateY(' + xPos + 'deg) rotateX(' + yPos + 'deg) scaleZ(2)';
+    } else {
+      WRAPPER.style.transform = 'perspective(1000px) rotateY(0deg) rotateX(0deg) scaleZ(1)';
+    }
+  }
+}
+
+scaleCheckbox.addEventListener('change', setupScale);
+
+// function movingMobileVideo(event) {
+//   if (scaleCheckbox.checked) {
+//     let tiltX = event.beta;
+//     let tiltY = event.gamma;
+//     let rotateX = (tiltX / 45) * -30;
+//     let rotateY = (tiltY / 45) * 30;
+
+//     WRAPPER.style.transform = 'perspective(1000px) rotateY(' + rotateY + 'deg) rotateX(' + rotateX + 'deg) scaleZ(2)';
+//   }
+// }
+
+// BODY.addEventListener('mousemove', setScale);
+// BODY.addEventListener('deviceorientation', movingMobileVideo);
+
+// Start
+// const srcRegex = /^(http:|https:|file:).*/;
+const srcRegex = /^(file:).*/;
+
 function startVideo() {
-  let videoSRC = VIDEO.src; 
-  if (videoSRC === '' || videoSRC.includes('undefined')) {
+  // if (!VIDEO.src || VIDEO.src === '' || VIDEO.src.length === 0 || typeof VIDEO.src === 'undefined') {
+  // if (srcRegex.test(VIDEO.src) || VIDEO.src === '' || !VIDEO.hasAttribute('src')) {
+  if (!VIDEO.hasAttribute('src') || VIDEO.src === '') {
     openButton.focus();
     openButton.classList.add('header__menu--error');
     setTimeout(() => {
       openButton.classList.remove('header__menu--error');
     }, 2000);
-  } else if (videoSRC && VIDEO.readyState >= VIDEO.HAVE_CURRENT_DATA) {
+  } else if (VIDEO.readyState >= VIDEO.HAVE_CURRENT_DATA) {
     openButton.classList.remove('header__menu--error');
     STARTBUTTON.classList.add('video__start--hide');
     CONTROLS.classList.remove('control--off', 'control--hide');
 
     VIDEO.play();
-    
+
     stayFocus();
     getStatistics();
   }
@@ -682,19 +889,19 @@ const tabs = SETTINGS.querySelectorAll('.settings__tab');
 tabButtons.forEach(button => {
   button.addEventListener('click', () => {
     tabButtons.forEach(btn => btn.classList.remove('settings__button--active'));
-    // tabs.forEach(tab => tab.classList.remove('settings__tab--active'));
     tabs.forEach(tab => {
       tab.classList.remove('settings__tab--active');
-      tab.classList.remove('settings__tab--relative');
+      tab.classList.remove('settings__tab--scroll');
       tab.removeAttribute('tabIndex');
     });
 
-    const tabName = button.getAttribute('data-tab');
-
     button.classList.add('settings__button--active');
+
+    const tabName = button.getAttribute('data-tab');
     document.querySelector(`.settings__tab[data-tab="${tabName}"]`).classList.add('settings__tab--active');
     document.querySelector(`.settings__tab[data-tab="${tabName}"]`).setAttribute('tabIndex', '0');
     document.querySelector(`.settings__tab[data-tab="${tabName}"]`).focus();
+
     updateSettingsHeight();
   });
 });
@@ -709,13 +916,12 @@ function updateSettingsHeight() {
 
   if (BODY.clientWidth < 1440) {
     settingsWrapper.style.height = `calc(100vh - ${settingsButtonHeight}px - 61px)`;
-    settingsWrapper.style.margin = '0';
 
-    if (activeTabHeight > settingsWrapperHeight) {
-      activeTab.classList.add('settings__tab--relative');
-      settingsWrapper.style.height = activeTabHeight + 'px';
-      settingsWrapper.style.margin = '15px 0 40px 0';
-    } 
+    if (activeTabHeight >= settingsWrapperHeight) {
+      activeTab.style.height = settingsWrapperHeight + 'px';
+      activeTab.classList.add('settings__tab--scroll');
+      activeTab.style.height = settingsWrapperHeight + 'px';
+    }
   }
 }
 
@@ -726,6 +932,8 @@ window.addEventListener('resize', updateSettingsHeight);
 // Video
 let progressInterval;
 let playbackQuality;
+let currentVideoPassed;
+let currentVideoLeft;
 
 function startProgress() {
   progressInterval = setTimeout(updateProgress, 1000);
@@ -739,10 +947,10 @@ function updateProgress() {
   statisticsBuffer.innerHTML = videoBuffer;
 
   // Duration
-  let currentVideoPassed = formatTime(videoCurrentTime); 
-  let currentVideoLeft = formatTime(videoDuration - videoCurrentTime); 
-  videoPassed.innerHTML = currentVideoPassed; 
-  videoLeft.innerHTML = currentVideoLeft; 
+  currentVideoPassed = formatTime(videoCurrentTime);
+  currentVideoLeft = formatTime(videoDuration - videoCurrentTime);
+  videoPassed.innerHTML = currentVideoPassed;
+  videoLeft.innerHTML = currentVideoLeft;
 
   startProgress();
   getTime();
@@ -773,33 +981,22 @@ const videoLeft = CONTROLS.querySelector('.control__time--left');
 
 function resetDuration() {
   VIDEORANGE.value = '0';
-
-  videoPassed.innerHTML = formatTime(0); 
-  videoLeft.innerHTML = formatTime(0); 
+  videoPassed.innerHTML = formatTime(0);
+  videoLeft.innerHTML = formatTime(0);
+  line.style.width = '0%';
+  line.value = 0;
 }
 
 function formatTime(timeInSeconds) {
   let hours = Math.floor(timeInSeconds / 3600);
   let minutes = Math.floor((timeInSeconds - (hours * 3600)) / 60);
   let seconds = Math.floor(timeInSeconds - (hours * 3600) - (minutes * 60));
-  
+
   hours = hours < 10 ? '0' + hours : hours;
   minutes = minutes < 10 ? '0' + minutes : minutes;
   seconds = seconds < 10 ? '0' + seconds : seconds;
 
   return hours + ':' + minutes + ':' + seconds;
-
-// if (hours < 10) {
-//   hours = '0' + hours;
-// }
-
-// if (minutes < 10) {
-//   minutes = '0' + minutes;
-// }
-
-// if (seconds < 10) {
-//   seconds = '0' + seconds;
-// }
 }
 
 // Video handler
@@ -827,15 +1024,14 @@ VIDEO.addEventListener('ended', endVideo);
 function errorVideo() {
   WRAPPER.classList.add('video__wrapper--error');
 
-  errorText.innerHTML = 'Помилка відео';
-  showError();
+  showError('Помилка відео &#128528;');
 }
 
 function removeErrorVideo() {
   WRAPPER.classList.remove('video__wrapper--error');
 }
 
-VIDEO.addEventListener('error', errorVideo);
+// VIDEO.addEventListener('error', errorVideo);
 VIDEO.addEventListener('loadeddata', removeErrorVideo);
 
 // Pause
@@ -850,34 +1046,3 @@ function removePauseAnimation() {
 VIDEO.addEventListener('pause', pauseAnimation);
 VIDEO.addEventListener('playing', removePauseAnimation);
 VIDEO.addEventListener('ended', removePauseAnimation);
-
-// 3D transform
-function movingVideo(event) {
-  if (scaleCheckbox.checked) {
-    let xPos = -(event.pageX / window.innerWidth - 0.5) * -20;
-    let yPos = (event.pageY / window.innerHeight - 0.5) * -20;
-    let blockRect = VIDEO.getBoundingClientRect();
-    let mouseX = event.clientX - blockRect.left;
-    let mouseY = event.clientY - blockRect.top;
-  
-    if (!(mouseX >= 0 && mouseX < blockRect.width && mouseY >= 0 && mouseY < blockRect.height)) {
-      WRAPPER.style.transform = 'perspective(1000px) rotateY(' + xPos + 'deg) rotateX(' + yPos + 'deg) scaleZ(2)';
-    }
-  } else {
-    WRAPPER.style.transform = 'none';
-  }
-}
-
-// function movingMobileVideo(event) {
-//   if (scaleCheckbox.checked) {
-//     let tiltX = event.beta;
-//     let tiltY = event.gamma;
-//     let rotateX = (tiltX / 45) * -30;
-//     let rotateY = (tiltY / 45) * 30;
-  
-//     WRAPPER.style.transform = 'perspective(1000px) rotateY(' + rotateY + 'deg) rotateX(' + rotateX + 'deg) scaleZ(2)';
-//   }
-// }
-
-BODY.addEventListener('mousemove', movingVideo);
-// BODY.addEventListener('deviceorientation', movingMobileVideo);
